e7313292eb1d9ffe73d5c7b6735d5a1030e7c3cb
Olga Madejska <madejska@amazon.com>
2013-10-12 16:28:09 +0200

    Added themes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012130700) do
+ActiveRecord::Schema.define(version: 20131012134122) do
 
   create_table "cards", force: true do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20131012130700) do
     t.string   "message"
     t.string   "image"
     t.string   "sound"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "themes", force: true do |t|
+    t.string   "name"
+    t.string   "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
