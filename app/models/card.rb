d3048a5def303a2e14944c53442a0a00c2f73ca1
Olga Madejska <madejska@amazon.com>
2013-10-13 11:43:12 +0200

    stuff done

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,26 +13,37 @@ class Card < ActiveRecord::Base
 		track = tracks.at(random_track)
 		embed_info = client.get('/oembed', :url => track.permalink_url)
 		self.sound = embed_info['html']
+		self.track_id = track.id
 	end
 
-	def get_images(tags='birthday')
+	
+	def get_images(tags='birthdaycake, geburtstagskind, baloons')
 		EyeEmConnector.configure do |config| config.client_id = 'ZZZ'
 			config.client_secret = 'AAA'
 			config.access_token = 'AAA'
 		end
-		results = EyeEmConnector.get('search', {:q=>"birthday", :includeAlbums=>1, :limit=>50})
-		b = results.env[:body]
-		a = b["albums"]
-		i = a["items"]
 
-		sorted = i.sort_by {|likes| likes["totalLikers"] }
+		items = []
+		tag_array = tags.split(",")
+		tag_array.each do |query|
+			query.strip!
+			results = EyeEmConnector.get('search', {:q=>query, :includeAlbums=>1, :limit=>50})
+			b = results.env[:body]
+			a = b["albums"]
+			i = a["items"]
+			items.concat(i)
+		end
+
+		sorted = items.sort_by {|likes| likes["totalLikers"] }
+		nr = 10
+		if sorted.length < 10
+			nr = sorted_length
+		end
 		random_image = rand(10)
 		item = sorted.at(sorted.length - random_image)
 		array = item["thumbUrl"].split("/200/")
 
 		new_string = array[0] + "/500/" + array[1]
-
 		self.image = new_string
-
 	end
 end
